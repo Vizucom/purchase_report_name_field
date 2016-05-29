406012413103e86f8a9cc2b4b3a525a5da22b3f2
Timo Talvitie <timo.talvitie@vizucom.com>
2016-05-29 21:02:57 +0300

    Initial commit

---
+++ __openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   Copyright (c) 2016- Vizucom Oy (http://www.vizucom.com)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Purchase Analysis / PO Name Field',
+    'category': 'Purchase',
+    'version': '0.1',
+    'author': 'Vizucom Oy',
+    'website': 'http://www.vizucom.com',
+    'depends': ['purchase'],
+    'description': """
+Purchase Analysis / PO Name Field
+=================================
+ * Customize Purchase Analysis so that the PO name is shown as one of the fields
+
+""",
+    'data': [
+        #'view/sale_report.xml',
+        #'data/ir.filters.csv',
+    ],
+}
